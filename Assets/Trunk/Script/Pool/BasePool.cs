bd9f91bbdb64c45190e5709e9c4864cf88bc2847
tzy <316166671@qq.com>
2020-01-06 22:09:11 +0800

    fast pool

---
+++ Assets/Trunk/Script/Pool/BasePool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BasePool<T>
+{
+
+    Queue<T> objectList = new Queue<T>();
+    public void Recycle(T obj)
+    {
+        OnRecycle(obj);
+        objectList.Enqueue(obj);
+    }
+
+    public T GetObj()
+    {
+        T result = default(T);
+        if (objectList != null && objectList.Count > 0)
+        {
+            result = objectList.Dequeue();
+            OnGet(result);
+        }
+        return result;
+    }
+    protected virtual void OnRecycle(T obj) { }
+    protected virtual void OnGet(T obj) { }
+}
