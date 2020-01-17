00cdd7649c9592e19617c20ea56eda128c3eed56
tzy <316166671@qq.com>
2020-01-17 20:16:07 +0800

    global

--- Assets/Trunk/Script/Global.cs
+++ Assets/Trunk/Script/Global.cs
@@ -5,22 +5,25 @@ using System.IO;
 using System;
 
 public class Global : MonoBehaviour {
+     Connection connection;
        // Use this for initialization
     void Awake ()
     {
         GameObject.DontDestroyOnLoad(gameObject);
         InitAppCfg();
+        InitConnection();
         InitModule();
 	}
-	
-	void Update ()
+  
+    void Update ()
     {
         //更新输入
         InputController.instance.FireCommand(InputCommand.UPDATE_INPUT);
     }
-
-
-
+    void InitConnection()
+    {
+        connection.Init();
+    }
 
     /// <summary>
     /// 初始化配置
