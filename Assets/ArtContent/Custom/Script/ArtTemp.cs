14d165a5249f462faa5d475aa6507d8592e78b79
raomuqi <raomuqi@qq.com>
2020-03-14 14:26:27 +0800

    others

--- Assets/ArtContent/Custom/Script/ArtTemp.cs
+++ Assets/ArtContent/Custom/Script/ArtTemp.cs
@@ -1,16 +1,27 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-//* 临时用来测试
+//* for testing
 public class ArtTemp : MonoBehaviour
 {
-    [Header("这个脚本是临时测试使用")]
+    [Header("For temporary test")]
     // Start is called before the first frame update
     public UnityEvent onEnable;
     public GameObject explosionGO;
     public float delayDestroy = 3;
+    //* tween state
+    private MeshRenderer meshRender;
+    MaterialPropertyBlock matBlock;
+    private Tweener tweenState;
+    private float stateIns = 0.0f;
+    void Start(){
+        meshRender = GetComponentInChildren<MeshRenderer>();
+        matBlock = new MaterialPropertyBlock();
+        tweenState = DOTween.To(() => stateIns, x => stateIns = x, 1, 1.2f).SetAutoKill(false)/*.SetEase(EaseFactory.StopMotion(5, Ease.OutFlash))*/.Pause();
+    }    
     void OnEnable()
     {
         if(onEnable != null) onEnable.Invoke();
@@ -24,4 +35,18 @@ public class ArtTemp : MonoBehaviour
     public void Destroy(){
         Destroy(gameObject, delayDestroy); 
     }
+
+    public void ActiveState()
+    {
+        if (meshRender == null) { Debug.LogError("mesh renderer missing"); return; }
+        tweenState.Rewind(false);
+           tweenState.Play().OnUpdate(delegate {
+               //Debug.LogWarning("stateIns");
+               matBlock.SetFloat("_StateIntensity", stateIns);
+               if(meshRender != null)meshRender.SetPropertyBlock(matBlock);
+           }).OnComplete(delegate {
+               tweenState.Rewind();
+           });
+    }
+
 }
