a5af8c4501248f90d966b2190d0aa7c1a77d8a7d
tzy <316166671@qq.com>
2020-04-12 13:11:38 +0800

    2P摄像机位置调整

---
+++ Assets/Trunk/Script/Module/Player/PlayerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform[] anchors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerModel playerModel = PlayerController.instance.GetModel<PlayerModel>(PlayerModel.name);
+        int index= playerModel.GetPlayerInfo().pos-1;
+        transform.SetParent(anchors[index]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
